3f3ce04512139beee7daa3c9ede415688ae23d28
Heisenberg <adrian.ipod25@gmail.com>
2014-09-11 06:35:40 +0200

    Maximum Vector
    Greedy solution

---
+++ Max_Vector.cpp
@@ -0,0 +1,27 @@
+/*
+ Principio de inclusión exclusión para vector Maximum subarray problem
+*/
+#include<stdio.h>
+#include<limits.h>
+#include<algorithm>
+#define MAX 1000005
+using namespace std;
+ 
+int mx,sum,N;
+int ar[MAX];
+ 
+int main(){
+	scanf("%d",&N);
+	for(int i=0;i<N;i++)
+		scanf("%d",&ar[i]);
+	sum = 0;
+	mx = INT_MIN;
+	for(int i=0;i<N;i++){
+		if((sum+ar[i])>=0)
+			sum += ar[i];
+		else sum = 0;
+		mx = max(sum,mx);
+	}
+	printf("%d\n",mx);
+	return 0;
+}
